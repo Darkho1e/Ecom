a37727c2416d6a3935ab855ea29a2dd36d28974b
Darkho1e <108342274+Darkho1e@users.noreply.github.com>
2025-04-29 13:22:06 +0300

    Delete tic_tac_toe/player.py

--- tic_tac_toe/player.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def get_player_info():
-    name = input("הכנס שם שחקן: ")
-    symbol = input(f"{name}, בחר X או O (או השאר ריק לקבלה אקראית): ").upper()
-    if symbol not in ['X', 'O']:
-        symbol = random.choice(['X', 'O'])
-        print(f"נבחר עבורך: {symbol}")
-    return name, symbol
